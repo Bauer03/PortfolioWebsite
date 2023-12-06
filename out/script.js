d18a3ac23e7829232db688cb03862695c41489ae
Paul Bauer <107049209+Bauer03@users.noreply.github.com>
2023-12-06 17:42:37 -0500

    updates
    pushing changes to portfolio for final

--- out/script.js
+++ out/script.js
@@ -10,6 +10,141 @@ var projectsLink = document.getElementById("projects-link");
 var projectsSection = document.getElementById("projects-section");
 var contactLink = document.getElementById("contact-link");
 var contactSection = document.getElementById("contact-section");
+var aboutTopHr = document.querySelector(".about-top-hr");
+var contactHr = document.querySelector(".contact-hr");
+document.addEventListener("DOMContentLoaded", function () {
+    var projGallery = document.querySelector(".proj-gallery");
+    var prevBtn = document.querySelector(".previous");
+    var nextBtn = document.querySelector(".next");
+    // Array of projects (similar to the previous example)
+    var projects = [
+        {
+            name: "firstProj",
+            markup: `
+            <div class="proj-left">
+              <div class="proj-left-top">
+                <video class="proj-video" muted="true" src="Images/Screen Recording 2023-11-30 at 3.06.20 PM.mov" loop="true" autoplay="true"></video>
+              </div>
+              <div class="proj-left-bottom">
+                <div class="proj-name">
+                  Knowledge Chase
+                </div>
+                <div class="proj-desc">
+                  Webapp created for Bridgewater College's Math
+                  & Computer Science department. Completed in collaboration with  
+                  faculty & small group of classmates.
+                </div>
+              <div class="proj-link">
+              <a href="https://kchase.xyz/index.html" target="_blank">
+              View the project here
+              <span class="material-symbols-outlined">open_in_new</span>
+              </a>
+              </div>
+              </div>
+              </div>
+              <div class="proj-right">
+              <div class="proj-timeline">
+              <div class="proj-timeline-title">
+              Timeline:
+              </div>
+              <div class="proj-timeline-body">
+              April-August 2023
+              </div>
+              <div class="proj-timeline-body">
+              Wireframes/Flow charts completed by end of April
+              </div>
+              <div class="proj-timeline-body">
+              Frontend development completed in early September, after start of 
+              classes.
+              </div>
+              </div>
+              <div class="proj-takeaways">
+              <div class="proj-takeaways-title">
+              Skills/Takeaways:
+              </div>
+              <div class="proj-takeaways-body">
+              Introducted to Typescript, learned to work with APIs
+              </div>
+                <div class="proj-takeaways-body">
+                Became familiar with Postman, MongoDB Compass
+                </div>
+              </div>
+            </div>
+          `
+        },
+        {
+            name: "secondProj",
+            markup: `
+                            <div class="proj-left">
+                                <div class="proj-left-top">
+                                  <img class="proj-video" src="Images/proj1.png" alt="Web Design Project 1">
+                                </div>
+                                <div class="proj-left-bottom">
+                                    <div class="proj-name">
+                                        Yamaha Reimagined
+                                    </div>
+                                    <div class="proj-desc">
+                                        Website created for my Web Design/Development class
+                                        in fall of 2023. Re-design of the Yamaha website for the
+                                        MT-09 bike. Completed solo.
+                                    </div>
+                                    <div class="proj-link">
+                                        <a href="https://github.com/Bauer03/webDesignProj1" target="_blank">
+                                            View the project here
+                                            <span class="material-symbols-outlined">open_in_new</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="proj-right">
+                                <div class="proj-timeline">
+                                    <div class="proj-timeline-title">
+                                        Timeline:
+                                    </div>
+                                    <div class="proj-timeline-body">
+                                        September 2023
+                                    </div>
+                                    <div class="proj-timeline-body">
+                                        The development for this project took course over
+                                        about two weeks, from start to finish.
+                                    </div>
+                                </div>
+                                <div class="proj-takeaways">
+                                    <div class="proj-takeaways-title">
+                                        Skills/Takeaways:
+                                    </div>
+                                    <div class="proj-takeaways-body">
+                                        Introducted to single-section websites
+                                    </div>
+                                    <div class="proj-takeaways-body">
+                                        Started becoming familiar with responsive design
+                                    </div>
+                                </div>
+                            </div>
+          `
+        }
+    ];
+    // Initialize the currently displayed project
+    var currentProjectIndex = 0;
+    // Function to update projGallery's innerHTML based on the current project
+    function updateGallery() {
+        projGallery.innerHTML = projects[currentProjectIndex].markup;
+    }
+    // Event listener for the "previous" button
+    prevBtn.addEventListener("click", function () {
+        // Decrement the current project index
+        currentProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
+        // Update the gallery
+        updateGallery();
+    });
+    // Event listener for the "next" button
+    nextBtn.addEventListener("click", function () {
+        // Increment the current project index
+        currentProjectIndex = (currentProjectIndex + 1) % projects.length;
+        // Update the gallery
+        updateGallery();
+    });
+});
 let currentTheme = 'dark';
 window.onload = function () {
     currentTheme = localStorage.getItem("mytheme") || 'light';
@@ -51,26 +186,29 @@ homeLink.addEventListener('click', () => {
     homeSection.scrollIntoView({ behavior: "smooth" });
     setTimeout(() => {
         closeNavMenu();
-    }, 50);
+    }, 100);
 });
 aboutLink.addEventListener('click', () => {
     aboutSection.scrollIntoView({ behavior: "smooth" });
     setTimeout(() => {
         closeNavMenu();
-    }, 50);
+        aboutTopHr.classList.add("opened");
+    }, 100);
 });
 projectsLink.addEventListener('click', () => {
     projectsSection.scrollIntoView({ behavior: "smooth" });
     setTimeout(() => {
         closeNavMenu();
-    }, 50);
+    }, 100);
 });
 contactLink.addEventListener('click', () => {
     contactSection.scrollIntoView({ behavior: "smooth" });
     setTimeout(() => {
         closeNavMenu();
-    }, 50);
+        contactHr.classList.add("opened");
+    }, 100);
 });
+var horizontalRules = document.querySelectorAll(".hr");
 var navLinks = document.querySelectorAll('.navLinkInner');
 // if the navLink clicked doesn't have class "active", remove class active from all other navLinks and add it to the one clicked. 
 navLinks.forEach(function (navLink) {
@@ -81,19 +219,33 @@ navLinks.forEach(function (navLink) {
             });
             navLink.classList.add('active');
         }
+        horizontalRules.forEach(function (hr) {
+            hr.classList.remove("opened");
+        });
     });
 });
+var lightbulb = document.querySelector(".lightbulb");
 function setToLightTheme() {
     document.body.classList.remove("dark");
     document.body.classList.add("light");
     currentTheme = 'light';
     localStorage.setItem("mytheme", currentTheme);
-    // console.log("Attempting to set theme to light")
+    lightbulb.textContent = "toggle_on";
+    console.log('theme was dark, going light');
 }
 function setToDarkTheme() {
     document.body.classList.remove("light");
     document.body.classList.add("dark");
     currentTheme = 'dark';
     localStorage.setItem("mytheme", currentTheme);
-    // console.log("Attempting to set theme to dark")
+    lightbulb.textContent = "toggle_off";
+    console.log('theme was light, going dark');
 }
+lightbulb.addEventListener('click', () => {
+    if (lightbulb.textContent == "toggle_on") {
+        setToDarkTheme();
+    }
+    else if (lightbulb.textContent == "toggle_off") {
+        setToLightTheme();
+    }
+});
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,4 @@
 // Please make sure you're not accessing this page on Safari! :)
-
 var navMenu = document.querySelector(".nav-menu") as HTMLElement;
 var menuToggle = document.getElementById("menu-toggle") as HTMLElement;
 
@@ -11,6 +10,148 @@ var projectsLink = document.getElementById("projects-link") as HTMLElement;
 var projectsSection = document.getElementById("projects-section") as HTMLElement;
 var contactLink = document.getElementById("contact-link") as HTMLElement;
 var contactSection = document.getElementById("contact-section") as HTMLElement;
+
+var aboutTopHr = document.querySelector(".about-top-hr") as HTMLElement;
+var contactHr = document.querySelector(".contact-hr") as HTMLElement;
+
+document.addEventListener("DOMContentLoaded", function() {
+  var projGallery = document.querySelector(".proj-gallery") as HTMLElement;
+  var prevBtn = document.querySelector(".previous") as HTMLElement;
+  var nextBtn = document.querySelector(".next") as HTMLElement;
+
+  // Array of projects (similar to the previous example)
+  var projects = [
+      {
+          name: "firstProj",
+          markup: `
+            <div class="proj-left">
+              <div class="proj-left-top">
+                <video class="proj-video" muted="true" src="Images/Screen Recording 2023-11-30 at 3.06.20 PM.mov" loop="true" autoplay="true"></video>
+              </div>
+              <div class="proj-left-bottom">
+                <div class="proj-name">
+                  Knowledge Chase
+                </div>
+                <div class="proj-desc">
+                  Webapp created for Bridgewater College's Math
+                  & Computer Science department. Completed in collaboration with  
+                  faculty & small group of classmates.
+                </div>
+              <div class="proj-link">
+              <a href="https://kchase.xyz/index.html" target="_blank">
+              View the project here
+              <span class="material-symbols-outlined">open_in_new</span>
+              </a>
+              </div>
+              </div>
+              </div>
+              <div class="proj-right">
+              <div class="proj-timeline">
+              <div class="proj-timeline-title">
+              Timeline:
+              </div>
+              <div class="proj-timeline-body">
+              April-August 2023
+              </div>
+              <div class="proj-timeline-body">
+              Wireframes/Flow charts completed by end of April
+              </div>
+              <div class="proj-timeline-body">
+              Frontend development completed in early September, after start of 
+              classes.
+              </div>
+              </div>
+              <div class="proj-takeaways">
+              <div class="proj-takeaways-title">
+              Skills/Takeaways:
+              </div>
+              <div class="proj-takeaways-body">
+              Introducted to Typescript, learned to work with APIs
+              </div>
+                <div class="proj-takeaways-body">
+                Became familiar with Postman, MongoDB Compass
+                </div>
+              </div>
+            </div>
+          `
+      },
+      {
+          name: "secondProj",
+          markup: `
+                            <div class="proj-left">
+                                <div class="proj-left-top">
+                                  <img class="proj-video" src="Images/proj1.png" alt="Web Design Project 1">
+                                </div>
+                                <div class="proj-left-bottom">
+                                    <div class="proj-name">
+                                        Yamaha Reimagined
+                                    </div>
+                                    <div class="proj-desc">
+                                        Website created for my Web Design/Development class
+                                        in fall of 2023. Re-design of the Yamaha website for the
+                                        MT-09 bike. Completed solo.
+                                    </div>
+                                    <div class="proj-link">
+                                        <a href="https://github.com/Bauer03/webDesignProj1" target="_blank">
+                                            View the project here
+                                            <span class="material-symbols-outlined">open_in_new</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="proj-right">
+                                <div class="proj-timeline">
+                                    <div class="proj-timeline-title">
+                                        Timeline:
+                                    </div>
+                                    <div class="proj-timeline-body">
+                                        September 2023
+                                    </div>
+                                    <div class="proj-timeline-body">
+                                        The development for this project took course over
+                                        about two weeks, from start to finish.
+                                    </div>
+                                </div>
+                                <div class="proj-takeaways">
+                                    <div class="proj-takeaways-title">
+                                        Skills/Takeaways:
+                                    </div>
+                                    <div class="proj-takeaways-body">
+                                        Introducted to single-section websites
+                                    </div>
+                                    <div class="proj-takeaways-body">
+                                        Started becoming familiar with responsive design
+                                    </div>
+                                </div>
+                            </div>
+          `
+      }
+  ];
+
+  // Initialize the currently displayed project
+  var currentProjectIndex = 0;
+
+  // Function to update projGallery's innerHTML based on the current project
+  function updateGallery() {
+      projGallery.innerHTML = projects[currentProjectIndex].markup;
+  }
+
+  // Event listener for the "previous" button
+  prevBtn.addEventListener("click", function() {
+      // Decrement the current project index
+      currentProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
+      // Update the gallery
+      updateGallery();
+  });
+
+  // Event listener for the "next" button
+  nextBtn.addEventListener("click", function() {
+      // Increment the current project index
+      currentProjectIndex = (currentProjectIndex + 1) % projects.length;
+      // Update the gallery
+      updateGallery();
+  });
+});
 
 let currentTheme = 'dark';
 window.onload = function() {
@@ -63,30 +204,33 @@ homeLink.addEventListener('click', () => {
   homeSection.scrollIntoView({ behavior: "smooth"});
   setTimeout(() => {
     closeNavMenu();
-  }, 50);
+  }, 100);
 })
 
 aboutLink.addEventListener('click', () => {
   aboutSection.scrollIntoView({ behavior: "smooth"});
   setTimeout(() => {
     closeNavMenu();
-  }, 50);
+    aboutTopHr.classList.add("opened");
+  }, 100);
 })
 
 projectsLink.addEventListener('click', () => {
   projectsSection.scrollIntoView({ behavior: "smooth"});
   setTimeout(() => {
     closeNavMenu();
-  }, 50);
+  }, 100);
 })
 
 contactLink.addEventListener('click', () => {
   contactSection.scrollIntoView({ behavior: "smooth"}); 
   setTimeout(() => {
     closeNavMenu();
-  }, 50);
+    contactHr.classList.add("opened");
+  }, 100);
 })
 
+var horizontalRules = document.querySelectorAll(".hr");
 var navLinks = document.querySelectorAll('.navLinkInner');
 // if the navLink clicked doesn't have class "active", remove class active from all other navLinks and add it to the one clicked. 
 navLinks.forEach(function(navLink) {
@@ -97,15 +241,21 @@ navLinks.forEach(function(navLink) {
         });
         navLink.classList.add('active');
       }
+      horizontalRules.forEach(function(hr) {
+        hr.classList.remove("opened")
+      })
     });
 });
+
+var lightbulb = document.querySelector(".lightbulb") as HTMLElement
 
 function setToLightTheme() {
   document.body.classList.remove("dark");
   document.body.classList.add("light");
   currentTheme = 'light';
   localStorage.setItem("mytheme", currentTheme);
-  // console.log("Attempting to set theme to light")
+  lightbulb.textContent = "toggle_on";
+  console.log('theme was dark, going light');
 }
 
 function setToDarkTheme() {
@@ -113,5 +263,14 @@ function setToDarkTheme() {
   document.body.classList.add("dark");
   currentTheme = 'dark';
   localStorage.setItem("mytheme", currentTheme);
-  // console.log("Attempting to set theme to dark")
+  lightbulb.textContent = "toggle_off";
+  console.log('theme was light, going dark');
 }
+
+lightbulb.addEventListener('click', () => {
+  if(lightbulb.textContent == "toggle_on") {
+    setToDarkTheme();
+  } else if(lightbulb.textContent == "toggle_off") {
+    setToLightTheme();
+  }
+});
